5ceeddd9bf499ba35a73497eea26b3fd80c761d0
awaiskhan1 <awaisk120@gmail.com>
2017-01-20 18:07:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function showElement(currentSet){
+	let count = 0,text=".", ul = document.createElement("ul");
+	ul.className += "ElmenentPrint";
+	
+	for (i=0; i<currentSet.elements.length, currentSet.elements[i].getAttribute("type")!== "button";i++)
+	{
+		count++;
+		if(currentSet.elements[i].value == null || currentSet.elements[i].value == ""){
+			text += "not set the value";
+		}
+		else{
+			 let li = document.createElement("li");
+			  textnode = document.createTextNode("Your are Enter the value in " + currentSet.elements[i].name + '= ' + 
+			  	currentSet.elements[i].value + "</b>.");
+			  li.appendChild(textnode);
+			  ul.appendChild(li);
+		}
+	}
+	text += "<br> <br>has "+ count + "Total tags  <br><br> ------------------" ;
+	currentSet.appendChild(ul);
+}
